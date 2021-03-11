0bdfa97c512639dd3510f8d5b34e1ffb8481022e
dusanstankovic84 <duxbih@outlook.com>
2021-03-11 17:49:56 +0100

    loopsadd

--- loops.js
+++ loops.js
@@ -1,5 +1,20 @@
 var i = 0;
 while (i<10) {
-    console.lo(i);
+    console.log(i);
     i++;
 }
+
+//Write a program that will iterate from 0 to 10 and display squares of numbers.
+var x = 0;
+while (x<=10) {
+    console.log(x*x);
+    x++
+}
+for (var x = 0; x <= 10; x++) {
+    console.log(x * x);
+}
+   
+    
+
+/* 1. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+number is odd or even, and display a message to the screen.*/
---
+++ loops2.js
@@ -0,0 +1,10 @@
+/* 1. Write a program that checks if a given element e is in the array a.
+Input: e = 3, a = [5, -4.2, 3, 7]
+Output: yes
+Input: e = 3, a = [5, -4.2, 18, 7]
+Output: no*/
+
+var e = 3;
+var a = [5, -4, 2, 3, 7];
+ 
+
