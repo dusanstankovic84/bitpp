a4eebc5e985428e9356dc5bec6fe95fe897580f4
dusanstankovic84 <duxbih@outlook.com>
2021-07-10 13:01:12 +0200

    exercise

--- OOPexercises/festival.js
+++ OOPexercises/festival.js
@@ -1,1 +1,116 @@
-"use strict"
+'use strict';
+
+(function () {
+
+    function Genre (name) {
+        this.name = name
+        this.getData = function () {
+            var arr;
+            var firstAndLastLetter;
+            arr = this.name.split('');
+            return firstAndLastLetter = arr[0].toUpperCase() + arr[arr.length - 1].toUpperCase();
+        }
+    }
+
+    function Movie (title, genre, length){
+        this.title = title
+        this.genre = genre
+        this.length = length
+        this.getData = function () {
+            return this.title + ', ' + this.length + 'min, ' + this.genre.getData();
+        }
+    }
+
+    function Program (date) {
+        this.date = new Date(date)
+        this.totalNumOfMovies = 0
+        this.listOfMovies = []
+        this.addMovie = function (movie) {
+            if (!movie || !(movie instanceof Movie)) {
+                throw new Error ('Invalid movie input')
+            }
+            this.listOfMovies.push(movie);
+        }
+        this.totalNumOfMovies = function () {
+            return this.listOfMovies.length;
+        }
+        this.getData = function () {
+            var year = this.date.getFullYear();
+            var month = this.date.getMonth() + 1;
+            var day = this.date.getDate();
+            var count = 0;
+            var movieLength = '\t' + day + '.' + month + '.' + year + ', program duration ';
+            this.listOfMovies.forEach(function (mov) {
+                count += mov.length;
+            }) 
+            movieLength += count + 'min';
+            this.listOfMovies.forEach(function (element) {
+                movieLength += '\n\t\t' + element.getData();
+                return movieLength;
+            }) 
+            return movieLength;
+        }
+    }
+
+    function Festival (name) {
+        this.name = name
+        this.numberOfMovies = 0
+        this.listOfPrograms = []
+        this.addProgram = function (program) {
+            if (!program || !(program instanceof Program)) {
+                throw new Error ('Invalid program input')
+            }
+            this.listOfPrograms.push(program)
+        }
+        this.numberOfMovies = function () {
+            var sum = 0;
+            this.listOfPrograms.forEach(function (movie) {
+                sum += movie.totalNumOfMovies();
+            });
+            return sum;
+        }
+        this.getData = function () {
+            var programFestival = this.name + ' has ' + this.numberOfMovies() + ' movie titles \n'; 
+            this.listOfPrograms.forEach(function(element) {
+                programFestival += element.getData() + '\n';
+            })
+            return programFestival;
+        }
+    }
+
+    function createMovie (movieTitle, movieLength, movieGenre) {
+        var genre = new Genre (movieGenre);
+        var movie = new Movie (movieTitle, genre, movieLength);
+
+        return movie;
+    }
+
+    function createProgram (date) {
+        var program = new Program (date);
+        return program;
+    }
+
+
+    var festival_1 = new Festival ('Weekend festival');
+
+    var program_1 = createProgram ('10 28 2017');
+    var program_2 = createProgram ('10 29 2017');
+
+    var movie_1 = createMovie ('Spider-Man: Homecoming', 133, 'Action');
+    var movie_2 = createMovie ('War for the Planet of the Apes', 140, 'Drama');
+    var movie_3 = createMovie ('The Dark Tower', 95, 'Adventure');
+    var movie_4 = createMovie ('Deadpool', 108, 'Comedy');
+    var movie_5 = createMovie ('Tom and Jerry', 85, 'Cartoon');
+
+    program_1.addMovie(movie_1);
+    program_1.addMovie(movie_2);
+    program_1.addMovie(movie_3);
+    program_2.addMovie(movie_4);
+    program_2.addMovie(movie_5);
+
+    festival_1.addProgram(program_1);
+    festival_1.addProgram(program_2);
+
+    console.log(festival_1.getData());
+
+})();
--- ecmaScript.js
+++ ecmaScript.js
@@ -1,5 +1,19 @@
 " use strict"
 
+///////////////////////////////////
+/*
+a.reduce(F)	 Vraća agregirajuću vrednost. Za svaki elemenat poziva funkciju F 
+             i prosleđuje joj ukupni dotadašnji rezultat i sam elemenat.
+
+a.every(F)	Validira elemente niza i vraća true ako su svi "ispravni". 
+            Za svaki elemenat poziva funkciju F koja vraća true ako je "ispravan".
+
+a.some(F)	Validira elemente niza i vraća true ako je bar jedan "ispravan". 
+            Za svaki elemenat poziva funkciju F koja vraća true ako je "ispravan".
+
+*/
+/////////////////////////////////////
+
 
 /* 1. Write a function that capitalizes the first letter of each string argument it receives.
 Function arguments: ["hello", "there", "ES&", 6]
@@ -82,6 +96,8 @@ const increase = (array, value) => {
 console.log(increase(array, 2));
 
 
+
+
 /* 4. Write a function that filters all even elements of the array.
 Input: [6, 11, 9, 0, 3]
 Output: [6, 0] */
@@ -90,7 +106,7 @@ let array = [6, 11, 9, 0, 3];
 
 
 const filter = (array) => array.filter(element => element % 2 === 0);
-    
+
 
 
 console.log(filter(array));
@@ -104,10 +120,10 @@ Output: [&#39;babel.js, &#39;JS standard&#39;] */
 const array = ["compiler", "transpiler", "babel.js", "JS standard", "linter"];
 let newArray = [];
 
-let filter = array =>  array.filter(element => {
-       
+let filter = array => array.filter(element => {
+
 })
-   
+
 
 let arrayCopy = [...array];
 
@@ -115,20 +131,64 @@ let arrayCopy = [...array];
 console.log(filter(array));
 
 
+
 /* 6. Write a function that filters all integer numbers from the given array.
 Input: [1.6, 11.34, 9.23, 7, 3.11, 8]
-
 Output: [7, 8] */
+
+let array = [1.6, 11.34, 9.23, 7, 3.11, 8];
+let integerNumbers = [];
+
+let filterNumbers = array => array.filter(element => {
+    if (element - Math.round(element) === 0) {
+        integerNumbers.push(element);
+        return integerNumbers;
+    }
+})
+
+console.log(filterNumbers(array));
+
 
 
 /* 7. Write a function that filters all integer arguments that contain digit 5.
-Function arguments: 23, 11.5, 9, &#39;abc&#39;, 45, 28, 553
+Function arguments: 23, 11.5, 9, "abc", 45, 28, 553
 Output: [45, 553] */
+
+
+let result = [];
+
+let numContainNumber5 = (...args) => args.filter(el => {
+    if (el - Math.round(el) === 0) {
+        let num = el.toString();
+        for (let i = 0; i < num.length; i++) {
+            if (num[i] === '5') {
+                result.push(el);
+                return result;
+            }
+        }
+    }
+})
+
+let func = numContainNumber5(23, 11.5, 9, 'abc', 45, 28, 553);
+console.log(func);
+
 
 
 /* 8. Write a function that returns indexes of the elements greater than 10.
 Input: [1.6, 11.34, 29.23, 7, 3.11, 18]
 Output: 1, 2, 5 */
+
+let indexes = '';
+
+let numGreaterThan10 = arr => arr.forEach(num => {
+    if (num > 10) {
+        indexes += `${arr.indexOf(num)} `;
+    }
+})
+
+numGreaterThan10([1.6, 11.34, 29.23, 7, 3.11, 18]);
+console.log(indexes);
+
 
 
 /* 9.
@@ -138,6 +198,52 @@ b. Write a function that prints out the names of persons older than 25.
 c. Write a function that check if there is a person older than 40.
 d. Write a function that checks if all persons are older than 20. */
 
+let persons = [{name: 'Nikola', age: 30}, {name: 'John', age: 15}, {name: 'Alex', age: 34}];
+
+/////
+let personNameOlderThan25 = arr => {
+    let older25 = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age > 25) {
+            older25 += `${arr[i].name} `
+        }
+    }
+    return older25;
+}
+
+/////
+let checkIfOlderThan40 = arr => {
+    let result;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age > 40) {
+            result = 'Yes'
+            break;
+        } else {
+            result = 'No'
+        }
+    }
+    return result;
+}
+
+////
+let checkAllOlderThan20 = arr => {
+    let res;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age < 20) {
+            res = `All persons aren't older than 20, some of persons are younger.`
+            break;
+        } else {
+            res = `Yes, all persons are older than 20.`
+        }
+    }
+    return res;
+}
+
+console.log(personNameOlderThan25(persons));
+console.log(checkIfOlderThan40(persons));
+console.log(checkAllOlderThan20(persons));
+
+
 
 
 /* 10. Write a function that checks if the given array is an array of positive integer values.
@@ -146,12 +252,43 @@ Output: yes
 Input: [3, -12, 4, 11]
 Output: no */
 
+const checkPositive = (arr) => {
+
+    let b = arr.every((args) => args > 0)
+    if (b == true) {
+        return 'yes';
+    }
+    return 'no';
+}
+
+console.log(checkPositive([3, 11, 9, 5, 6]));
+console.log(checkPositive([3, -12, 4, 11]));
+
+
 
 /* 11. Write a function that calculates the product of the elements of the array.
 Input: [2, 8, 3]
 Output: 48 */
 
+let array = [2, 8, 3];
+
+const multiplyElements = array => array.reduce((a, b) => a * b, 1)
+const result = multiplyElements(array);
+
+console.log(result)
+
+
 
 /*12. Write a function that calculates the maximum of the given array.
 Input: [2, 7, 3, 8, 5.4]
 Output: 8 */
+
+let array = [2, 7, 3, 8, 5.4, 11];
+
+const findMaximum = array => Math.max(...array);
+const result = findMaximum(array);
+
+console.log(result);
+
+
+
